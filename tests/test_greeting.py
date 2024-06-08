3132a3dd8a21c55e58d63aefd9cf7b4e4676a30f
nastya0908 <nas20040908@gmail.com>
2024-06-08 16:01:25 +0300

    Add tests for greeting.py

---
+++ tests/test_greeting.py
@@ -0,0 +1,7 @@
+from src.greeting import greeting
+
+
+def test_greeting(capsys):
+    greeting("Nastya")
+    out, _ = capsys.readouterr()
+    assert out == "Hello, Nastya!\nOkey. Let me try to calculate example you enter!\n\n"
